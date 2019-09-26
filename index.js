165ba2787ad0697598b8e2145f9255a220f9cb8b
Eugen Kondratiev <kondratiev.es@gmail.com>
2019-09-26 23:01:27 +0300

    sum of all players and length of search results

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 require('./scheduler')();
-const COMMAND_HELP = `
-/find имя_часть_имени  - поиск игрока по имени
-/exot id_нации  - поиск игроков нужной нации( пока по номеру)
-/next - следующая порция игроков
-/prev - прошлая порция игроков
-/start - старт
-/xxx - рестарт`;
+
 
 const dbPool = require('./model/connection-pool');
-const Find = require('./model/find-by-name');
+const Find = require('./model/pefl-searcher');
 
 const Telegraf = require('telegraf')
 const Extra = require('telegraf/extra')
@@ -23,8 +17,16 @@ async function startUp() {
   try {
     await require('./model/get-players-table')();
     await require('./model/get-clubs-table')();
+
      bot.use(Telegraf.log())
-    ;
+    const COMMAND_HELP = `
+/find имя_часть_имени  - поиск игрока по имени (напр. /find хиса)
+/exot id_нации  - поиск игроков нужной нации( пока по номеру) (напр /exot 190)
+/next - следующая порция игроков
+/prev - прошлая порция игроков
+/start - старт
+/xxx - рестарт
+Всего игроков - ${global.playersBase.length}\n`;
 
     bot.hears(/\/find ([\sA-Яа-яЁёЪъ]+)/i, (ctx) => {
       const _id = ctx.message.from.id;
@@ -32,6 +34,7 @@ async function startUp() {
       // console.log("ctx.tg - ", ctx.tg);
       // console.log("ctx.match - ",ctx.match);
       const nameToFind = ctx.match[1];
+      ctx.nameToFind = nameToFind;
       if (nameToFind.length < 3) return ctx.reply(`Пожалуйста, вводите 3 и более букв`    )
       // nameToFind.splice(0, 6);
       console.log("Find request heard", nameToFind);
@@ -46,9 +49,10 @@ async function startUp() {
       // console.log("ctx.tg - ", ctx.tg);
       console.log("ctx.match - ",ctx.match);
       const nationToFind = ctx.match[1];
+      ctx.nationToFind = nationToFind;
       console.log("Exot request heard", nationToFind);
       const resp = ctx.db[_id].findByNation(nationToFind);
-      console.log('resp - ', resp)
+      console.log('resp - ', resp);
       return ctx.replyWithHTML(resp, {parse_mode : "HTML"})
     })
     bot.command('next', (ctx) => {
--- model/pefl-searcher.js
+++ model/pefl-searcher.js
@@ -4,7 +4,7 @@ const pefl = 'http://pefl.ru/';
 class PeflSearcher {
     constructor(_playersBase) {
         this.playersBase = _playersBase;
-        this.findByNameResponse = [];
+        this.searchResponse = [];
         this.viewPosition = 0;
         this.portion = 10;
     }
@@ -15,7 +15,7 @@ class PeflSearcher {
             ['t', 'k'],
             ['j', _j],
             ['z', _z],
-            ]);;
+            ]);
         return clubURL.toString();    
     }
 
@@ -26,19 +26,24 @@ class PeflSearcher {
         if (!Array.isArray(rows) || (rows.length == 0)) {
             return "<b> - </b>  "; 
         }
-        const resp = rows.reduce( (tbl,row, _index) => 
-            tbl + (this.viewPosition + _index + 1) + '. '
-                + row.reduce((tr,cel, index) =>
+        const resp = `Найдено ${this.searchResponse.length} совпадений\n`
+            + rows.reduce( (tbl,row, _index) => 
+                tbl + (this.viewPosition + _index + 1) + '. '
+                 + row.reduce((tr,cel, index) =>
                     { let element = "";
                     switch(index) {
                         case 0:  
                         case 2:  
-                        case 3:
                             element = "<b>"+ cel + "</b>  "; 
+                            break;
+                        case 3:
+                            element = cel == 'GK' ? "<b>"+ cel + "</b>  " : (cel + " "); 
                             break;
                         case 1:                
                         case 6:
-                            element = "<i>"+ global.nationBase[parseInt(cel)][1] + "</i>  "; 
+                            element = cel == '-1' 
+                            ? "<i>-CA-</i>  "
+                            : "<i>"+ global.nationBase[parseInt(cel)][1] + "</i>  "; 
                             break;
                         case 4:             
                             
@@ -50,6 +55,12 @@ class PeflSearcher {
                                     : ('<a href="'+ this.getClubUrl(global.clubsBase[parseInt(cel)][0], global.clubsBase[parseInt(cel)][2]) 
                                 + '">' + global.clubsBase[parseInt(cel)][1] + '</a>  '); 
                             break;
+                        case 7:
+                                element = cel == '' 
+                                        ? '- ' 
+                                        : ('<a href="'+ pefl + cel 
+                                    + '">ссылка</a>  '); 
+                                break;
                         default:
                             element = cel + "  "; 
                         }
@@ -61,7 +72,7 @@ class PeflSearcher {
          
     }
     getPlayersPortion(){
-        const respPortion = this.findByNameResponse.filter(
+        const respPortion = this.searchResponse.filter(
             (pl, index) => index >= this.viewPosition && index < (this.viewPosition + this.portion)
         );
             console.log("i= ", this.viewPosition, " -- ",  respPortion);
@@ -71,24 +82,24 @@ class PeflSearcher {
     
     findByName(nameSubString) {
         this.viewPosition = 0;
-        this.findByNameResponse = this.playersBase.filter(pl => pl[0].toLowerCase().includes(nameSubString.toLowerCase()));
-        console.log("findByNameResponse  - ", this.findByNameResponse);
-        return this.getTableRow(this.findByNameResponse.length < (this.portion + 1)  
-                ? this.findByNameResponse
+        this.searchResponse = this.playersBase.filter(pl => pl[0].toLowerCase().includes(nameSubString.toLowerCase()));
+        console.log("searchResponse  - ", this.searchResponse);
+        return this.getTableRow(this.searchResponse.length < (this.portion + 1)  
+                ? this.searchResponse
                 : this.getPlayersPortion()
         )        
     }
     findByNation(_nation) {
         this.viewPosition = 0;
-        this.findByNameResponse = this.playersBase.filter(pl => pl[1] == _nation);
-        return this.getTableRow( this.findByNameResponse.length < (this.portion + 1)  
-                ? this.findByNameResponse
+        this.searchResponse = this.playersBase.filter(pl => pl[1] == _nation);
+        return this.getTableRow( this.searchResponse.length < (this.portion + 1)  
+                ? this.searchResponse
                 : this.getPlayersPortion()
         )
     }
     
     getNextPortionOfPlayers() {
-        if (this.findByNameResponse.length > this.viewPosition + this.portion ) this.viewPosition += this.portion;
+        if (this.searchResponse.length > this.viewPosition + this.portion ) this.viewPosition += this.portion;
         return this.getTableRow(this.getPlayersPortion());
     }
     getPrevPortionOfPlayers() {
